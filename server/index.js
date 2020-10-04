c99d70bb1b704a5a0ee5593571f446b0b05e47a2
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-10-04 20:41:20 +0200

    Workflow test.

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(__dirname + "/public/"))
 
+app.use(/.*/, () => __dirname + "public/index.html")
 
-const port = process.env.PORT || 3000;
-app.listen(port, () =>console.log(`Server started at port: ${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started at port: ${port}`));
