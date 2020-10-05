1df27cb4cbf3951cf2349a238ed6b4d80fc52948
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-10-05 21:55:24 +0200

    - Changed - Server runs on localhost:8000

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(__dirname + "/public/"))
 
+// Put your routes here...
+
 app.use(/.*/, () => __dirname + "public/index.html")
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server started at port: ${port}`));
