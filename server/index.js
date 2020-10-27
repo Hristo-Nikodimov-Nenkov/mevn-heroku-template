a72380dd5a8f26d7512d7c7d8f8544151e6c726d
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-10-27 12:02:08 +0100

    - Added - bodyParser to app pipeline.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const app = require("express")();
 
 const {database, routes, server} = require("./configs");
+const {bodyParser} = require("./middlewares");
 
 database();
+
+bodyParser(app);
+
 routes(app);
 server(app);
