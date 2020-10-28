7e2696bb3ab6f4b6170876ab3c1a888c9fb76af3
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-10-28 15:33:14 +0100

    - Added - AppFooter component.

---
+++ client/src/components/common/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <hr/>
+    MEVN-Heroku-Template is template project under MIT license.
+    <strong>If you wish to use it for commercial purpose please contact me.</strong>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "AppFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
