8ea479a0bc9a766ecbef1f6b8ddbde625a527bf3
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-10-28 15:32:57 +0100

    - Added - NavMenu component.

---
+++ client/src/components/common/NavMenu.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav>
+    <div v-for="(link, index) in links" :key="index">
+      <router-link :to="link.url">{{ link.text }}</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavMenu",
+  data() {
+    return {
+      links: [
+        {
+          text: "Home",
+          url: "/"
+        },
+        {
+          text: "About",
+          url: "/about"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+nav {
+  background-color: #cccccc;
+  padding: 5px 15px;
+}
+
+@media screen and (max-width: 500px) {
+  nav {
+    display: flex;
+    flex-direction: column;
+    align-content: space-between;
+  }
+}
+
+@media screen and (min-width: 501px) {
+  nav {
+    display: flex;
+    justify-content: space-around;
+    background-color: #cccccc;
+    padding: 5px 15px;
+  }
+}
+</style>
