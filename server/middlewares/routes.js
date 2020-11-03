ee6d2cf77a8ee65961e2321b2c0bbc7ae37bbcaa
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-11-03 17:40:14 +0100

    - Added - account router for "/account" to routes.

--- server/middlewares/routes.js
+++ server/middlewares/routes.js
@@ -1,9 +1,11 @@
 const {common} = require("./../configs");
+const {account} = require("./../routers");
 
 const fallback = (req, res) => {
     res.sendFile(common.indexHtml);
 }
 
 module.exports = function (app) {
+    app.use("/account", account);
     app.use(/.*/, fallback);
 }
