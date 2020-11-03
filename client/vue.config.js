142a298cfec51ec38e5d5ce0ff40de59c082c532
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-11-03 16:33:20 +0100

    - Added - development proxy for /account routes.

--- client/vue.config.js
+++ client/vue.config.js
@@ -4,6 +4,9 @@ module.exports = {
     outputDir: path.resolve(__dirname, "../server/public"),
     devServer:{
         proxy:{
+            "/account":{
+                target:"http://localhost:5000"
+            },
             "/api":{
                 target:"http://localhost:5000"
             }
