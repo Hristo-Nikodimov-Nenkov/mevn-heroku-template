88d6073a87e6e2d68322aca0f0ce55b5b818bb10
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-11-17 17:56:59 +0100

    - Added - GET /logout and GET /profile.

--- server/routers/account.js
+++ server/routers/account.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
-const {account} = require("./../services");
+const {account} = require("./../controllers");
+const {security} = require("./../services");
 
 router.post("/register", account.register);
 router.post("/login", account.login);
+router.get("/logout", security.authenticated, account.logout);
+router.get("/profile", security.authenticated, account.profile);
 
 module.exports = router;
