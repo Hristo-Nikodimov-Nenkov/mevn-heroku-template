e89c135498b366ccedccca3a92d2ad2f76c65c86
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-11-17 17:52:28 +0100

    - Moved - authenticate middleware to security service. - Changed - authenticate middleware to use the method from security service.

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -1,31 +1,5 @@
-const token = require("jsonwebtoken");
-const {jwt, cookies} = require("./../configs");
-
-function authenticate(req, res, next) {
-    console.log(`Cookies = ${JSON.stringify(req.cookies)}`);
-    console.log(`SignedCookies = ${JSON.stringify(req.signedCookies)}`);
-
-    const authCookie = req.cookies[cookies.tokenCookieName] || req.signedCookies[cookies.tokenCookieName];
-
-    if (authCookie) {
-        token.verify(
-            authCookie,
-            jwt.secret,
-            (err, payload) => {
-                if (err) {
-                    console.log(err);
-                }
-
-                if (payload) {
-                    req.user = payload;
-                }
-            }
-        )
-    }
-
-    next();
-}
+const {security} = require("./../services");
 
 module.exports = function (app) {
-    app.use(authenticate);
+    app.use(security.authenticate);
 }
