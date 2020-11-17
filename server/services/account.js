d856c96d4f6dc7b4834dbb80ba8b812739f91c2e
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-11-17 17:55:33 +0100

    - Changed - register to registerAsync. - Changed - registration implementation. - Changed - login to loginAsync. - Changed - login implementation. - Added - mapToViewModel function - not exported.

--- server/services/account.js
+++ server/services/account.js
@@ -1,29 +1,59 @@
-function register(req, res) {
-    res.status(200).send(JSON.stringify(req.user));
+const User = require("./../models/User");
+
+async function registerAsync(model) {
+    try {
+        if (await User.usernameExists(model.username)) {
+            
+            throw `Username ${model.username} is already registered.`;
+        }
+        if (await User.emailExists(model.email)) {
+            throw `E-Mail ${model.email} is already registered.`
+        }
+        if (model.phoneNumber && await User.phoneNumberExists(model.phoneNumber)) {
+            throw `Phone number ${model.phoneNumber} is already registered.`
+        }
+
+        const user = new User({
+            username: model.username,
+            email: model.email,
+            phoneNumber: model.phoneNumber || ""
+        });
+
+        user.setPassword(model.password);
+
+        const dbUser = await user.save();
+        return mapToViewModel(dbUser);
+    } catch (err) {
+        throw err;
+    }
 }
 
-const jsonwebtoken = require("jsonwebtoken");
-const {jwt, cookies} = require("./../configs");
+async function loginAsync(model) {
+    try {
+        let user =
+            await User.getByUsername(model.username) ||
+            await User.getByEmail(model.username);
 
-function login(req, res) {
-    jsonwebtoken.sign(
-        req.body,
-        jwt.secret,
-        jwt.options,
-        (err, token) => {
-            if(err){
-                console.log(err);
-            }
+        if (!user.checkPassword(model.password)) {
+            throw "Invalid credentials";
+        }
 
-            if (token) {
-                res.cookie(cookies.tokenCookieName, token, cookies.options);
-            }
+        return mapToViewModel(user);
+    } catch (err) {
+        throw err;
+    }
 
-            res.status(200).send(req.body);
-        })
+}
+
+function mapToViewModel(user) {
+    return {
+        username: user.username,
+        email: user.email,
+        phoneNumber: user.phoneNumber
+    }
 }
 
 module.exports = {
-    register,
-    login
+    registerAsync,
+    loginAsync
 }
