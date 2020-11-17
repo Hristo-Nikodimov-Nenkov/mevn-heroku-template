590869f6258df81d75bed28087178915bbcdc27f
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-11-17 17:49:15 +0100

    - Changed - jwt in /configs.

--- server/configs/jwt.js
+++ server/configs/jwt.js
@@ -1,17 +1,18 @@
 const configs = {
     secret: "My JWT signing secret.",
-    issuer: "localhost:5000",
-    audience: "localhost:5000",
-    options:{},
-    expiry: "24h"
+    options: {
+        issuer: "localhost:5000",
+        audience: "localhost:5000",
+        expiresIn: "24h"
+    }
 }
 
 function getConfigs() {
     if (process.env.NODE_ENV === "production") {
         configs.secret = process.env.JWT_SECRET;
-        configs.issuer = process.env.BASE_URL;
-        configs.audience = process.env.BASE_URL;
-        configs.expiry = process.env.JWT_EXPIRY;
+        configs.options.issuer = process.env.BASE_URL;
+        configs.options.audience = process.env.BASE_URL;
+        configs.options.expiresIn = process.env.JWT_EXPIRY;
     }
 
     return configs;
