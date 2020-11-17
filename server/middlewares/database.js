afd78d97b09296dd9964c61288280fadad7de4d1
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-11-17 17:51:25 +0100

    - Changed - database middleware.

--- server/middlewares/database.js
+++ server/middlewares/database.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
 const {database} = require("./../configs");
 
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+};
+
 module.exports = function () {
     const connectionString = database.getConnectionString(database.connection);
 
-    mongoose.connect(connectionString,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        })
-        .then((db) => {
-            console.log(`Connected to ${ database.connection.databaseName}`);
+    mongoose.connect(connectionString, options)
+        .then(() => {
+            console.log(`Connected to ${database.connection.databaseName}`);
+
+            require("./../models/User");
         })
         .catch((err) => {
             console.log(err);
